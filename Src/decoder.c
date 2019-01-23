72ee07afcff78fad68932acb3fce1d7097270a7b
Alistair Bill <alistair.bill@gmail.com>
2019-01-23 11:10:38 +0000

    Add different decoding modes

--- Src/decoder.c
+++ Src/decoder.c
@@ -5,7 +5,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define MAX_CHANGES 67
+#define MAX_CHANGES 68
 
 extern TIM_HandleTypeDef htim3;
 
@@ -27,6 +27,8 @@ static const protocol_t protocols[] = {
 };
 static const unsigned int protocols_len = sizeof(protocols) / sizeof(protocols[0]);
 
+static unsigned int codes[4];
+static unsigned int code_count = 0;
 static const int receive_tolerance = 60;
 static bool initialized = false;
 static bool ignore_pulse = false;
@@ -34,6 +36,16 @@ static bool ignore_pulse = false;
 static volatile unsigned int change_count = 0;
 static volatile unsigned int repeat_count = 0;
 static volatile unsigned int timings[MAX_CHANGES];
+static volatile unsigned int rolling_buffer = 0;
+static volatile unsigned int pulse_width = 0;
+
+
+static void setup_interrupt_handler_pre(void);
+static void rolling_interrupt_handler_post(void);
+static void noop(void);
+
+void (*interrupt_handler_pre)(void) = &setup_interrupt_handler_pre;
+void (*interrupt_handler_post)(void) = &noop;
 
 void Decoder_Init(void)
 {
@@ -42,13 +54,20 @@ void Decoder_Init(void)
     initialized = true;
 }
 
-static inline unsigned int diff(int a, int b)
+static inline unsigned int diff(const int a, const int b)
 {
     return abs(a - b);
 }
 
-static void send_message(unsigned int value, int p)
+static void send_message(const unsigned int value, const int p)
 {
+    if (code_count < 4) {
+        codes[code_count++] = value;
+        if (code_count == 4) {
+            interrupt_handler_pre = &noop;
+            interrupt_handler_post = &rolling_interrupt_handler_post;
+        }
+    }
     switch(p) {
         case 0:
             // PT2240: 20 address bits followed by 4 data bits
@@ -57,7 +76,7 @@ static void send_message(unsigned int value, int p)
     }
 }
 
-static bool receive_protocol(const int p, unsigned int changes)
+static bool setup_receive_protocol(const int p, const unsigned int changes)
 {
     protocol_t protocol;
     memcpy(&protocol, &protocols[p], sizeof(protocol_t));
@@ -86,6 +105,60 @@ static bool receive_protocol(const int p, unsigned int changes)
     return false;
 }
 
+static bool decode_bit_rolling(const int p)
+{
+    protocol_t protocol;
+    memcpy(&protocol, &protocols[p], sizeof(protocol_t));
+    if (diff(timings[change_count] / protocol.one.high, timings[change_count + 1] / protocol.one.low) < 250) {
+        rolling_buffer <<= 1;
+        rolling_buffer |= 1;
+        return true;
+    } else if (diff(timings[change_count] / protocol.zero.high, timings[change_count + 1] / protocol.zero.low) < 250) {
+        rolling_buffer <<= 1;
+        return true;
+    }
+    return false;
+}
+
+static void noop(void)
+{
+    return;
+}
+
+static void rolling_interrupt_handler_post(void)
+{
+    if (change_count % 2 == 1) {
+        int i = 0;
+        for (i = 0; i < protocols_len; i++) {
+            if (decode_bit_rolling(i)) {
+                break;
+            }
+        }
+        if ((rolling_buffer & 0xffffff) == codes[0] || (rolling_buffer & 0xffffff) == codes[1]) {
+            send_message(rolling_buffer & 0xffffff, i);
+            rolling_buffer = 0;
+        }
+    }
+}
+
+static void setup_interrupt_handler_pre(void)
+{
+    if (pulse_width > separation_limit) {
+        if (diff(pulse_width, timings[0]) < 400) {
+            repeat_count++;
+            if (repeat_count == 2) {
+                for (int i = 0; i < protocols_len; i++){
+                    if (setup_receive_protocol(i, change_count)) {
+                        break;
+                    }
+                }
+                repeat_count = 0;
+            }
+        }
+        change_count = 0;
+    }
+}
+
 void Decoder_Edge_Callback(void)
 {
     if (!initialized) {
@@ -97,8 +170,8 @@ void Decoder_Edge_Callback(void)
         return;
     }
 
-    uint32_t pulse_width = __HAL_TIM_GetCounter(&htim3);
-    if (pulse_width >= 100) {
+    pulse_width = __HAL_TIM_GetCounter(&htim3);
+    if (pulse_width >= 400) {
         __HAL_TIM_SetCounter(&htim3, 0);
     } else {
         // short pulse
@@ -106,24 +179,12 @@ void Decoder_Edge_Callback(void)
         ignore_pulse = true;
         return;
     }
+    (*interrupt_handler_pre)();
 
-    if (pulse_width > separation_limit) {
-        if (diff(pulse_width, timings[0]) < 400) {
-            repeat_count++;
-            if (repeat_count == 2) {
-                for (int i = 0; i < protocols_len; i++){
-                    if (receive_protocol(i, change_count)) {
-                        break;
-                    }
-                }
-                repeat_count = 0;
-            }
-        }
-        change_count = 0;
-    }
     if (change_count >= MAX_CHANGES) {
         change_count = 0;
         repeat_count = 0;
     }
     timings[change_count++] = pulse_width;
+    (*interrupt_handler_post)();
 }
