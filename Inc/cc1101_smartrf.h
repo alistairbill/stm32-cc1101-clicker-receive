9fc7ac7568a6f97d00785a13e3e3c36534efa0ff
Alistair Bill <alistair.bill@gmail.com>
2019-01-23 11:09:57 +0000

    Update register settings

--- Inc/cc1101_smartrf.h
+++ Inc/cc1101_smartrf.h
@@ -11,27 +11,27 @@ const uint8_t RF_SETTINGS[] = {
   0x91,  // SYNC0               Sync Word, Low Byte
   0xFF,  // PKTLEN              Packet Length
   0x04,  // PKTCTRL1            Packet Automation Control
-  0x32,  // PKTCTRL0            Packet Automation Control
+  0x12,  // PKTCTRL0            Packet Automation Control
   0x00,  // ADDR                Device Address
-  0x0A,  // CHANNR              Channel Number
+  0x07,  // CHANNR              Channel Number
   0x06,  // FSCTRL1             Frequency Synthesizer Control
   0x00,  // FSCTRL0             Frequency Synthesizer Control
   0x21,  // FREQ2               Frequency Control Word, High Byte
   0x62,  // FREQ1               Frequency Control Word, Middle Byte
   0x76,  // FREQ0               Frequency Control Word, Low Byte
-  0xF9,  // MDMCFG4             Modem Configuration
-  0x3B,  // MDMCFG3             Modem Configuration
+  0xC6,  // MDMCFG4             Modem Configuration
+  0x12,  // MDMCFG3             Modem Configuration
   0x30,  // MDMCFG2             Modem Configuration
-  0x00,  // MDMCFG1             Modem Configuration
-  0x60,  // MDMCFG0             Modem Configuration
-  0x15,  // DEVIATN             Modem Deviation Setting
+  0x20,  // MDMCFG1             Modem Configuration
+  0xF8,  // MDMCFG0             Modem Configuration
+  0x34,  // DEVIATN             Modem Deviation Setting
   0x07,  // MCSM2               Main Radio Control State Machine Configuration
   0x30,  // MCSM1               Main Radio Control State Machine Configuration
   0x18,  // MCSM0               Main Radio Control State Machine Configuration
-  0x16,  // FOCCFG              Frequency Offset Compensation Configuration
+  0x34,  // FOCCFG              Frequency Offset Compensation Configuration
   0x6C,  // BSCFG               Bit Synchronization Configuration
   0x03,  // AGCCTRL2            AGC Control
-  0x40,  // AGCCTRL1            AGC Control
+  0x00,  // AGCCTRL1            AGC Control
   0x91,  // AGCCTRL0            AGC Control
   0x87,  // WOREVT1             High Byte Event0 Timeout
   0x6B,  // WOREVT0             Low Byte Event0 Timeout
